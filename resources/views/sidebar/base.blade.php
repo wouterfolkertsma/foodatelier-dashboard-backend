9b5797e824c1943043479fb7baed1ff7f8bfc429
robin <reuhus@hs-stud.de>
2020-11-23 18:34:42 +0100

    gitflow-feature-stash: Navbar

--- resources/views/sidebar/base.blade.php
+++ resources/views/sidebar/base.blade.php
@@ -1,4 +1,4 @@
-<nav class="menu" tabindex="0">
+<div class="menu" tabindex="0">
     <div class="smartphone-menu-trigger"></div>
     <div class="avatar">
         <img src="https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg" />
@@ -10,4 +10,4 @@
     <div class="label">
         <img src="/../images/falabel.svg">
     </div>
-</nav>
+</div>
