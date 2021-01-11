5736c3efff966e79590b0a1761d1cd231780524d
Wouter <wouter.folkertsma@guapa.nl>
2021-01-11 09:36:24 +0100

    edit

--- reseed.sh
+++ reseed.sh
@@ -6,6 +6,3 @@ rm storage/app/storage/*
 rm storage/app/storage/avatars/*
 php artisan storage:link
 php artisan db:seed
-
-php artisan make:user --type=employee --name=admin --lastname=admin --email=test@foodatelier.nl --password=test
-php artisan make:user --type=client --name=client --lastname=client --email=client@foodatelier.nl --password=test
